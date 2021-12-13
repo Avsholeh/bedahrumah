8fdfe707b851b1d1aab12958ce6113dcd319c14c
avsholeh <avsholeh@gmail.com>
2021-12-13 20:32:55 +0700

    local push

--- bedahrumah.sql
+++ bedahrumah.sql
@@ -11,11 +11,95 @@
  Target Server Version : 50724
  File Encoding         : 65001
 
- Date: 11/12/2021 12:46:23
+ Date: 13/12/2021 20:32:36
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for data_gambar
+-- ----------------------------
+DROP TABLE IF EXISTS `data_gambar`;
+CREATE TABLE `data_gambar`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_rumah` int(11) UNSIGNED NULL DEFAULT NULL,
+  `jenis` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `file` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `fk_rumah_gambar`(`id_rumah`) USING BTREE,
+  CONSTRAINT `fk_rumah_gambar` FOREIGN KEY (`id_rumah`) REFERENCES `data_rumah` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of data_gambar
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for data_pengaju
+-- ----------------------------
+DROP TABLE IF EXISTS `data_pengaju`;
+CREATE TABLE `data_pengaju`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `no_ktp` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `no_kk` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `jenis_kelamin` int(11) NULL DEFAULT NULL,
+  `tempat_lahir` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `tgl_lahir` date NULL DEFAULT NULL,
+  `alamat` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `sektor_pekerjaan` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `penghasilan` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `pengeluaran` int(50) NULL DEFAULT NULL,
+  `status_pemilik_tanah` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `bukti_pemilik_tanah` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `status_pemilik_rumah` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `aset_rumah` int(11) NULL DEFAULT NULL,
+  `aset_tanah` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of data_pengaju
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for data_rumah
+-- ----------------------------
+DROP TABLE IF EXISTS `data_rumah`;
+CREATE TABLE `data_rumah`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_pengaju` int(11) UNSIGNED NULL DEFAULT NULL,
+  `pondasi` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `kolom_balok` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `konstruksi_atap` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `pencahayaan` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `ventilasi` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `mck` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `kondisi_mck` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `pembuangan` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `kondisi_pembuangan` int(11) NULL DEFAULT NULL,
+  `sumber_air_minum` int(11) NULL DEFAULT NULL,
+  `sumber_listrik` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `luas_rumah` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `jumlah_penghuni` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `tinggi_bangunan` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `ruangan_lainnya` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `material_atap` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `kondisi_atap` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `material_dinding` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `kondisi_dinding` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `material_lantai` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `kondisi_lantai` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `luas_lantai` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `fk_pengaju_rumah`(`id_pengaju`) USING BTREE,
+  CONSTRAINT `fk_pengaju_rumah` FOREIGN KEY (`id_pengaju`) REFERENCES `data_pengaju` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of data_rumah
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for users
@@ -26,13 +110,15 @@ CREATE TABLE `users`  (
   `nama_lengkap` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `aktif` int(10) NULL DEFAULT NULL,
+  `jenis` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES (1, 'Admin', 'admin@admin.com', '$2y$10$LRlTCz9fdw83JMExsFybnOU/ZItdKfVOoJFRMQgUUQJgNYzc6nGei');
-INSERT INTO `users` VALUES (12, 'Aliquip pariatur Re', 'welova@mailinator.com', '$2y$10$DzwO1i55/CTX6YPcHBxVk.lho8AmznS/bSLnd0VUHROsIss5te7Xi');
+INSERT INTO `users` VALUES (1, 'Mita', 'admin@admin.com', '$2y$10$LRlTCz9fdw83JMExsFybnOU/ZItdKfVOoJFRMQgUUQJgNYzc6nGei', 1, 'Admin');
+INSERT INTO `users` VALUES (2, 'Aliquip pariatur Re', 'welova@mailinator.com', '$2y$10$DzwO1i55/CTX6YPcHBxVk.lho8AmznS/bSLnd0VUHROsIss5te7Xi', 1, 'User');
 
 SET FOREIGN_KEY_CHECKS = 1;
